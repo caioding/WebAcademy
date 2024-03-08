5a471f8d0d000d1c58c3f59fd79ec33008ff0cd8
caioding <7caioc@gmail.com>
2024-03-08 07:06:30 -0400

    Labs de JavaScript resolvidos

--- Aulas de JS/Lab02/main.js
+++ Aulas de JS/Lab02/main.js
@@ -4,14 +4,12 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`];
 const alts = {
-  'pic1.jpg' : 'Guitarra 1',
-  'pic2.jpg' : 'Guitarra 2',
-  'pic3.jpg' : 'Guitarra 3'
+  'pic1.jpg': 'Guitarra 1',
+  'pic2.jpg': 'Guitarra 2',
+  'pic3.jpg': 'Guitarra 3'
 }
-
 
 for (const image of images) {
   const newImage = document.createElement('img');
@@ -27,11 +25,11 @@ for (const image of images) {
 btn.addEventListener('click', () => {
   const btnClass = btn.getAttribute('class');
   if (btnClass === 'dark') {
-    btn.setAttribute('class','light');
+    btn.setAttribute('class', 'light');
     btn.textContent = 'Clarear';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
   } else {
-    btn.setAttribute('class','dark');
+    btn.setAttribute('class', 'dark');
     btn.textContent = 'Escurecer';
     overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
