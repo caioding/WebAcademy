5dc34ff06c20742b3271644bc490088c029e648e
caioding <7caioc@gmail.com>
2024-04-04 14:32:03 -0400

    update

--- Express/ExpTS/src/index.ts
+++ Express/ExpTS/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import logger from "./middlewares/logger";
 import router from "./router/router";
 import { engine } from "express-handlebars";
-// import sass from "node-sass-middleware";
+import sass from "node-sass-middleware";
 
 dotenv.config();
 envalid();
@@ -23,23 +23,31 @@ app.set("views", `${__dirname}/views`);
 app.engine(
   "handlebars",
   engine({
-    helpers: require(`${__dirname}/views/helpers/helpers.ts`)
+    helpers: require(`${__dirname}/views/helpers/helpers.ts`),
+    layoutsDir: `${__dirname}/views/layouts`
   })
 );
 
 app.use("/html", express.static(`${__dirname}/../public/html`));
 app.use("/css", express.static(`${__dirname}/../public/css`));
-app.use("/js", express.static(`${__dirname}/../public/js`));
+app.use("/js", [
+  express.static(`${__dirname}/../public/js`),
+  express.static(`${__dirname}/../node_modules/bootstrap/dist/js`)
+]);
 app.use("/img", [express.static(`${__dirname}/../public/img`)]);
 
-// app.use(
-//   sass({
-//     src: `${__dirname}/../public/scss`,
-//     dest: `${__dirname}/../public/css`,
-//     outputStyle: "compressed",
-//     prefix: "/css"
-//   })
-// );
+app.use(
+  sass({
+    src: `${__dirname}/../public/scss`,
+    dest: `${__dirname}/../public/css`,
+    outputStyle: "compressed",
+    prefix: "/css"
+  })
+);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(logger("simples"));
+app.use(router);
 
 app.use(router);
 
--- Express/ExpTS/src/router/router.ts
+++ Express/ExpTS/src/router/router.ts
@@ -1,12 +1,21 @@
-import { Router } from 'express';
-import mainController from '../controllers/main';
+import { Router } from "express";
+import mainController from "../controllers/main";
+import produtoCrontroller from "../controllers/produto";
+
 const router = Router();
-// Main Controller
-router.get('/', mainController.index);
-router.get('/lorem/:paragraphs', mainController.lorem);
-router.get('/hb1', mainController.hb1);
-router.get('/hb2', mainController.hb2);
-router.get('/hb3', mainController.hb3);
-router.get('/hb4', mainController.hb4);
-// User Controller
+router.get("/", mainController.index);
+router.get("/lorem/:paragraphs", mainController.lorem);
+router.get("/hb1", mainController.hb1);
+router.get("/hb2", mainController.hb2);
+router.get("/hb3", mainController.hb3);
+router.get("/hb4", mainController.hb4);
+
+router.get("/produto", produtoCrontroller.index);
+router.get("/produto/create", produtoCrontroller.create);
+router.post("/produto/create", produtoCrontroller.create);
+router.get("/produto/:id", produtoCrontroller.read);
+router.get("/produto/update/:id", produtoCrontroller.update);
+router.post("/produto/update/:id", produtoCrontroller.update);
+router.get("/produto/remove/:id", produtoCrontroller.remove);
+
 export default router;
